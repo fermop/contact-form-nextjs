6f0213fe4951ce329917a695e432ba6e73204587
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2026-02-14 12:42:10 -0600

    feat(ui): setup global styles, typography, and layout structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Karla } from "next/font/google";
+import "./globals.css";
+
+const karla = Karla({
+  variable: "--font-karla",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Frontend Mentor | Contact from with Next.js",
+  description: "Contact us to get more details about our team!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${karla.variable} antialiased font-karla`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
