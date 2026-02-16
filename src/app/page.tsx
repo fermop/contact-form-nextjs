fb2aba0de2c40f6de45d529998d3252e712f8fd7
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2026-02-16 14:32:42 -0600

    fix: nest h1 element into main tag to fix accessibility report on FM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,13 @@ export default function Home() {
 
   return (
     <div>
-      <h1 className="sr-only">Frontend Mentor | Contact from with Next.js</h1>
-        <main className="min-h-dvh grid place-items-center px-4 py-6">
-          <Form setSuccess={setSuccess} />
+      <main className="min-h-dvh grid place-items-center px-4 py-6">
+        <h1 className="sr-only">Frontend Mentor | Contact from with Next.js</h1>
+        
+        <Form setSuccess={setSuccess} />
 
-          {success && <Toast onClose={() => setSuccess(false)} />}
-        </main>
+        {success && <Toast onClose={() => setSuccess(false)} />}
+      </main>
     </div>
   );
 }
