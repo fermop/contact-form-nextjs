ce28dfcbb494ebc93012f8622f173a7e9822c872
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2026-02-15 18:39:02 -0600

    feat(ui): add error messages for input form fields

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 export default function Form() {
+  const [ error, setError ] = useState(true)
+
   return (
     <section className='animate-fade-in-up w-full max-w-185 p-6 sm:p-10 bg-white text-neutral-grey-900 rounded-md m-auto shadow-xl'>
       <h2 className='text-3xl font-bold mb-8'>Contact Us</h2>
@@ -12,8 +15,9 @@ export default function Form() {
             <input
               id="firstname-input"
               type="text"
-              className='border border-neutral-grey-500 focus:border-primary-green-600 hover:border-primary-green-600 outline-0 rounded-lg px-6 py-3 cursor-pointer duration-300'
+              className={`border ${!error ? 'border-neutral-grey-500 focus:border-primary-green-600 hover:border-primary-green-600' : 'animate-shake border-red-400 focus:border-red-400 hover:border-red-400'} outline-0 rounded-lg px-6 py-3 cursor-pointer duration-300`}
             />
+            {error && <p className='animate-shake text-red-400'>This field is required</p>}
           </label>
 
           <label htmlFor="lastname-input" className='grid gap-2 cursor-pointer'>
@@ -21,8 +25,9 @@ export default function Form() {
             <input
               id="lastname-input"
               type="text"
-              className='border border-neutral-grey-500 focus:border-primary-green-600 hover:border-primary-green-600 outline-0 rounded-lg px-6 py-3 cursor-pointer duration-300'
+              className={`border ${!error ? 'border-neutral-grey-500 focus:border-primary-green-600 hover:border-primary-green-600' : 'animate-shake border-red-400 focus:border-red-400 hover:border-red-400'} outline-0 rounded-lg px-6 py-3 cursor-pointer duration-300`}
             />
+            {error && <p className='animate-shake text-red-400'>This field is required</p>}
           </label>
         </div>
 
@@ -31,8 +36,9 @@ export default function Form() {
           <input
             id="email-input"
             type="email"
-            className='border border-neutral-grey-500 focus:border-primary-green-600 hover:border-primary-green-600 outline-0 rounded-lg px-6 py-3 cursor-pointer duration-300'
+            className={`border ${!error ? 'border-neutral-grey-500 focus:border-primary-green-600 hover:border-primary-green-600' : 'animate-shake border-red-400 focus:border-red-400 hover:border-red-400'} outline-0 rounded-lg px-6 py-3 cursor-pointer duration-300`}
           />
+          {error && <p className='animate-shake text-red-400'>Please enter a valid email address</p>}
         </label>
 
         <div className='grid gap-2'>
@@ -49,6 +55,7 @@ export default function Form() {
               <span>Support Request</span>
             </label>
           </div>
+          {error && <p className='animate-shake text-red-400'>Please select a query type</p>}
         </div>
         
         <label htmlFor="message-area" className='grid gap-2 cursor-pointer'>
@@ -56,19 +63,23 @@ export default function Form() {
           <textarea 
             id="message-area" 
             name="message"
-            className='w-full h-64 sm:h-32 border border-neutral-grey-500 focus:border-primary-green-600 hover:border-primary-green-600 outline-0 rounded-lg px-6 py-3 cursor-pointer duration-300'
+            className={`resize-none w-full h-64 sm:h-32 border ${!error ? 'border-neutral-grey-500 focus:border-primary-green-600 hover:border-primary-green-600' : 'animate-shake border-red-400 focus:border-red-400 hover:border-red-400'} outline-0 rounded-lg px-6 py-3 cursor-pointer duration-300`}
           />
+          {error && <p className='animate-shake text-red-400'>This field is required</p>}
         </label>
         
-        <label htmlFor="consent-checkbox" className='flex gap-6 my-6 items-center cursor-pointer'>
-          <input 
-            id="consent-checkbox" 
-            name="message"
-            type='checkbox'
-            className='accent-primary-green-600 cursor-pointer duration-300'
-          />
-          <span>I consent to being contacted by the team <span className='text-primary-green-600'>*</span></span>
-        </label>
+        <div className='my-6 space-y-2'>
+          <label htmlFor="consent-checkbox" className='flex gap-6 items-center cursor-pointer'>
+            <input 
+              id="consent-checkbox" 
+              name="message"
+              type='checkbox'
+              className='accent-primary-green-600 cursor-pointer duration-300'
+            />
+            <span>I consent to being contacted by the team <span className='text-primary-green-600'>*</span></span>
+          </label>
+          {error && <p className='animate-shake text-red-400'>To submit this form, please conset to being contacted</p>}
+        </div>
 
         <button
           type='submit'
