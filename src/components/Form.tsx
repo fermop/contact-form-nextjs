2d7c85834bd21267d9b06f6e5028317180374c36
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2026-02-15 18:44:29 -0600

    fix(ui): add green bg-color for radio input types

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,7 +2,8 @@
 import React, { useState } from 'react'
 
 export default function Form() {
-  const [ error, setError ] = useState(true)
+  const [ error, setError ] = useState(false)
+  const [ checked, setChecked ] = useState(false)
 
   return (
     <section className='animate-fade-in-up w-full max-w-185 p-6 sm:p-10 bg-white text-neutral-grey-900 rounded-md m-auto shadow-xl'>
@@ -45,12 +46,12 @@ export default function Form() {
           <p className='cursor-pointer'>Query Type <span className='text-primary-green-600'>*</span></p>
           
           <div className='grid gap-4 sm:grid-cols-2'>            
-            <label htmlFor="general" className='flex gap-4 items-center border border-neutral-grey-500 focus:border-primary-green-600 hover:border-primary-green-600 outline-0 rounded-lg px-6 py-3 cursor-pointer duration-300'>
+            <label htmlFor="general" className={`flex gap-4 items-center ${checked && 'bg-primary-green-200 border-primary-green-600'} border border-neutral-grey-500 focus:border-primary-green-600 hover:border-primary-green-600 outline-0 rounded-lg px-6 py-3 cursor-pointer duration-300`}>
               <input type="radio" id='general' name='queryType' className='accent-primary-green-600 sm:w-5 sm:h-5'/>
               <span>General Enquiry</span>
             </label>
 
-            <label htmlFor="support" className='flex gap-4 items-center border border-neutral-grey-500 focus:border-primary-green-600 hover:border-primary-green-600 outline-0 rounded-lg px-6 py-3 cursor-pointer duration-300'>
+            <label htmlFor="support" className={`flex gap-4 items-center ${checked && 'bg-primary-green-200 border-primary-green-600'} border border-neutral-grey-500 focus:border-primary-green-600 hover:border-primary-green-600 outline-0 rounded-lg px-6 py-3 cursor-pointer duration-300`}>
               <input type="radio" id='support' name='queryType' className='accent-primary-green-600 sm:w-5 sm:h-5'/>
               <span>Support Request</span>
             </label>
